ba4f71375258ec77cae4b2833d42b8ac80487774
Hafizh <hafizhrahman12@gmail.com>
2022-01-15 14:26:48 +0700

    fix

--- errorc.go
+++ errorc.go
@@ -1,48 +1,47 @@
 package errorc
 
 import (
+	"errors"
 	"fmt"
 )
 
-type error interface {
+type errorc interface {
 	Code() int
 	Text() string
 	FullError() string
 }
 
 // custom error
-type errorC struct {
-	code int
-	text string
+type errorcS struct {
+	code  int
+	text  string
+	Error error
 }
 
 // new error without code
-func New(text string) error {
-	return &errorC{
+func New(text string) *errorcS {
+	return &errorcS{
 		text: text,
 	}
 }
 
 // new error with code, purposely for http response
-func NewWithCode(code int, text string) error {
-	return &errorC{
-		code: code,
-		text: text,
+func NewWithCode(code int, text string) *errorcS {
+	return &errorcS{
+		code:  code,
+		text:  text,
+		Error: errors.New(text),
 	}
 }
 
-func (e *errorC) Code() int {
+func (e *errorcS) Code() int {
 	return e.code
 }
 
-func (e *errorC) Text() string {
+func (e *errorcS) Text() string {
 	return e.text
 }
 
-func (e *errorC) FullError() string {
+func (e *errorcS) FullError() string {
 	return fmt.Sprintf("status: %d. error: %s", e.code, e.text)
-}
-
-func (e *errorC) Error() string {
-	return e.text
 }
--- errorc_test.go
+++ errorc_test.go
@@ -7,28 +7,28 @@ import (
 
 func Test__(t *testing.T) {
 	err := NewWithCode(200, "ok")
-	fmt.Println(err)
+	fmt.Println(err.Error)
 	fmt.Println(err.Code())
 	fmt.Println(err.Text())
 	fmt.Println(err.FullError())
 	fmt.Println()
 
 	err = NewWithCode(400, "bad request")
-	fmt.Println(err)
+	fmt.Println(err.Error)
 	fmt.Println(err.Code())
 	fmt.Println(err.Text())
 	fmt.Println(err.FullError())
 	fmt.Println()
 
 	err = NewWithCode(500, "internal server error")
-	fmt.Println(err)
+	fmt.Println(err.Error)
 	fmt.Println(err.Code())
 	fmt.Println(err.Text())
 	fmt.Println(err.FullError())
 	fmt.Println()
 
 	err = New("parameter nil")
-	fmt.Println(err)
+	fmt.Println(err.Error)
 	fmt.Println(err.Code())
 	fmt.Println(err.Text())
 	fmt.Println(err.FullError())
